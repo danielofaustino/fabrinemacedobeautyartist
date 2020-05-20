d66b4d48e9497756961e0ae8b8c22b32e6b3506a
Tecnologia02 <tecnologia02@intranet.local>
2020-05-20 16:59:13 -0300

    alterações

---
+++ js/script.js
@@ -0,0 +1,69 @@
+
+// CURSOS : 
+
+let trocandoImagem1 = (x) =>{
+    x.src="img/cursos/1-1.jpeg";
+}
+
+let voltandoImagem1 = (y) =>{
+    y.src="img/cursos/1.jpeg";
+}
+
+let trocandoImagem2 = (x) =>{
+    x.src="img/cursos/2-2.jpeg";
+}
+
+let voltandoImagem2 = (y) =>{
+    y.src="img/cursos/2.jpeg";
+}
+
+let trocandoImagem3 = (x) =>{
+    x.src="img/cursos/3-3.jpeg";
+}
+
+let voltandoImagem3 = (y) =>{
+    y.src="img/cursos/3.jpeg";
+}
+
+let trocandoImagem4 = (x) =>{
+    x.src="img/cursos/4-4.jpeg";
+}
+
+let voltandoImagem4 = (y) =>{
+    y.src="img/cursos/4.jpeg";
+}
+
+
+// PROCEDIMENTOS
+
+let trocandoImagemP1 = (x) =>{
+    x.src="img/procedimentos/1-1.jpeg";
+}
+
+let voltandoImagemP1 = (y) =>{
+    y.src="img/procedimentos/1.jpeg";
+}
+
+let trocandoImagemP2 = (x) =>{
+    x.src="img/procedimentos/2-2.jpeg";
+}
+
+let voltandoImagemP2 = (y) =>{
+    y.src="img/procedimentos/2.jpeg";
+}
+
+let trocandoImagemP3 = (x) =>{
+    x.src="img/procedimentos/3-3.jpeg";
+}
+
+let voltandoImagemP3 = (y) =>{
+    y.src="img/procedimentos/3.jpeg";
+}
+
+let trocandoImagemP4 = (x) =>{
+    x.src="img/procedimentos/4-4.jpeg";
+}
+
+let voltandoImagemP4 = (y) =>{
+    y.src="img/procedimentos/4.jpeg";
+}
